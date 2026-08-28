3edf5965ff8173d80a5372a863a2491f823b6bbb
EntropyXi <25804170@qq.com>
2026-08-28 14:43:42 +0800

    Disable excessive EDM sampling churn

--- diafno/models/diffusion.py
+++ diafno/models/diffusion.py
@@ -47,7 +47,7 @@ class ElucidatedDiffusion(nn.Module):
             rho=7,
             P_mean=-1.2,
             P_std=1.2,
-            S_churn=80,
+            S_churn=0,
             S_tmin=0.05,
             S_tmax=50,
             S_noise=1.003,
